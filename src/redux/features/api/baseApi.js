fe7e85cce155eda1875361e386c9e200b1467c53
ACM-Anik <anikmojumder5252@gmail.com>
2024-03-02 23:58:06 +0600

    endpoints inplemented and useGetPostQuery from baseApi exported

--- src/redux/features/api/baseApi.js
+++ src/redux/features/api/baseApi.js
@@ -2,11 +2,17 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';/* Reac
 
 
 const baseApi = createApi({
-    reducerPath:'api',
+    reducerPath: 'api',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://jsonplaceholder.typicode.com/'
+        baseUrl: 'https://jsonplaceholder.typicode.com'
     }),
-    endpoints: (builder)=>({}),
+    endpoints: (builder) => ({
+        getPosts: builder.query({
+            query: () => '/posts',
+        }),
+    }),
 });
+
+export const { useGetPostsQuery } = baseApi;
 
 export default baseApi;
