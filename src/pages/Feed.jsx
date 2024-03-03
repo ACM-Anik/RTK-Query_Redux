4987a7c296e5fd2bf0643c7dd01b5193aecd2ac8
ACM-Anik <anikmojumder5252@gmail.com>
2024-03-03 23:59:35 +0600

    react hook form installed and initial setup done and a input added

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,22 +1,34 @@
 import PostCard from "../components/layouts/PostCard";
 import { useGetPostByIdQuery, useGetPostsQuery } from "../redux/features/api/baseApi";
+import { useForm } from "react-hook-form";
 
 const Feed = () => {
 
     // const { data: posts, isLoading, isError, error } = useGetPostsQuery(); //* Returns an object
 
-    const {data: post, isLoading, isError, error} = useGetPostByIdQuery(1);
+    const { data: post, isLoading, isError, error } = useGetPostByIdQuery(1);
 
     if (isLoading) {
         return <p className="text-center my-auto text-8xl text-zinc-300">Loading...</p>
     }
 
-    if(!isLoading && isError) {
+    if (!isLoading && isError) {
         return <p className="text-8xl text-zinc-300">Something went wrong!</p>
     }
 
     // console.log('iserror', isError);
     // console.log('error', error);
+
+    const {
+        register,
+        handleSubmit,
+        // formState: { errors },
+    } = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data);
+    }
+
 
     return (
         <div>
@@ -24,6 +36,16 @@ const Feed = () => {
             <div className="flex flex-col gap-3">
                 {/* {posts?.map((post) => <PostCard key={post.id} post={post}></PostCard>)} */}
                 <PostCard key={post.id} post={post}></PostCard>
+
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    {/* <input className="" defaultValue="test" {...register("example")} /> */}
+
+                    <input className="" {...register("exampleRequired", { required: true })} />
+
+                    {/* {errors.exampleRequired && <span>This field is required</span>} */}
+
+                    <input type="submit" />
+                </form>
             </div>
         </div>
     )
