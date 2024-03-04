735d655ccbbc8291eb8806070814806f11a6df86
ACM-Anik <anikmojumder5252@gmail.com>
2024-03-04 22:42:49 +0600

    getPosts builder's structure changed for experiment

--- src/redux/features/api/baseApi.js
+++ src/redux/features/api/baseApi.js
@@ -7,15 +7,18 @@ const baseApi = createApi({
         baseUrl: 'https://jsonplaceholder.typicode.com'
     }),
     endpoints: (builder) => ({
-        getPosts: builder.query({
-            query: () => '/posts',
+        getPosts: builder.query({ 
+            query: () => ({
+                url: '/posts',
+            }),
         }),
         getUsers: builder.query({
-            query: () => '/users',
+            query: () => '/users', // get query "url" charao direct lekha jay.
         }), 
         getPostById: builder.query({ 
             query: (id) => `/posts/${id}`,// Only one parameter can send like (id). So best to send an object
-        })
+        }),
+
     }),
 });
 
